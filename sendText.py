be9831cb3ddcc72357ea425c6ca844e350def97b
Carl Block <cblock94@hotmail.com>
2017-09-30 07:52:22 -0700

    Football Folder added

--- sendText.py
+++
@@ -1,9 +0,0 @@
-from twilio.rest import TwilioRestClient
-
-def sendText(body):
-    accountSID = 'AC38fea5a3add1fb8dc2b3110457eb9ce5'
-    authToken = '95225502aabaea96493635aeae8dae1a'
-    twilioCli = TwilioRestClient(accountSID, authToken)
-    myTwilioNumber = '+14159657083'
-    myCellPhone = '+14157177149'
-    message = twilioCli.messages.create(body='{}'.format(body), from_=myTwilioNumber, to=myCellPhone)
